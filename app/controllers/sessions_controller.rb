df0ac093d259da716e8c269d97d0e71f51b93847
Justin Davidson <jutstindavidson23@gmail.com>
2016-06-06 15:27:03 +1000

    Added fields to user model and user forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,6 +22,6 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:session).permit(:email, :password)
+    params.require(:session).permit(:username, :password)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606043532) do
+ActiveRecord::Schema.define(version: 20160606045416) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
