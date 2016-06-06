b1b8e02e71584b55d694ff55bfd4c911ddac24fb
Justin Davidson <jutstindavidson23@gmail.com>
2016-06-06 14:49:36 +1000

    Added homepage, dashboard and basic user functionality

--- Gemfile
+++ Gemfile
@@ -7,6 +7,11 @@ gem 'rails', '>= 5.0.0.rc1', '< 5.1'
 gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
+
+
+gem 'monban-generators'
+
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+
+  root 'home#index'
+
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
